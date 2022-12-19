c178ca7d082a2a330920fe0471ef972d5c254fd7
russellbanares322 <banaresrussell11@gmail.com>
2022-12-19 22:05:17 +0800

    style: change tech stack skills style

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ import "aos/dist/aos.css";
 function About() {
   //Animate on Scroll
   useEffect(() => {
-    Aos.init({ duration: 2000 });
+    Aos.init();
   });
   return (
     <Container className="text-center" id="about">
@@ -18,10 +18,10 @@ function About() {
         ABOUT
       </h1>
       <Row>
-        <Col sm={6} data-aos="zoom-in">
+        <Col sm={4} data-aos="zoom-in">
           <Image className="about_image" src={bgAbout} fluid />
         </Col>
-        <Col sm={6}>
+        <Col sm={8}>
           <h6 className="about_text" data-aos="fade-up">
             Passionate junior front-end web developer, loves to learn new
             skills.
@@ -33,22 +33,64 @@ function About() {
             className="d-flex justify-content-center"
             direction="horizontal"
             data-aos="zoom-in-up"
-            gap={2}
+            gap={4}
           >
-            <div>
+            <div className="react_wrapper">
               <FaReact size={50} className="react_logo" />
+              <p
+                data-aos="fade-right"
+                data-aos-duration="1000"
+                style={{ paddingTop: "1rem" }}
+              >
+                ReactJS
+              </p>
             </div>
-            <div>
+            <div className="css_wrapper">
               <FaCss3Alt size={50} className="css_logo" />
+              <p
+                data-aos="fade-right"
+                data-aos-duration="1000"
+                style={{ paddingTop: "1rem" }}
+              >
+                CSS
+              </p>
             </div>
-            <div>
+            <div className="html_wrapper">
               <FaHtml5 size={50} className="html_logo" />
+              <p
+                data-aos="fade-right"
+                data-aos-duration="1000"
+                style={{ paddingTop: "1rem" }}
+              >
+                HTML
+              </p>
             </div>
-            <div>
+          </Stack>
+          <Stack
+            className="d-flex justify-content-center mt-4"
+            direction="horizontal"
+            data-aos="zoom-in-up"
+            gap={4}
+          >
+            <div className="js_wrapper">
               <IoLogoJavascript size={50} className="js_logo" />
+              <p
+                data-aos="fade-right"
+                data-aos-duration="1000"
+                style={{ paddingTop: "1rem" }}
+              >
+                Javascript
+              </p>
             </div>
-            <div>
+            <div className="firebase_wrapper">
               <SiFirebase size={50} className="firebase_logo" />
+              <p
+                data-aos="fade-right"
+                data-aos-duration="1000"
+                style={{ paddingTop: "1rem" }}
+              >
+                Firebase
+              </p>
             </div>
           </Stack>
         </Col>
