b4d426734bd9649b2db37ed70ff6b9458f276e84
russellbanares322 <banaresrussell11@gmail.com>
2023-01-25 21:38:36 +0800

    style: change project text font weight

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "react-toastify/dist/ReactToastify.css";
 import App from "./App";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter as Router } from "react-router-dom";
