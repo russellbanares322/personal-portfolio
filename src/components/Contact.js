cd6bc26809536c42f1de3959fdb844136f537caa
russellbanares322 <banaresrussell11@gmail.com>
2023-02-17 22:29:53 +0800

    feat: add form validation in contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,34 +11,50 @@ const { REACT_APP_SERVICE_ID, REACT_APP_TEMPLATE_ID, REACT_APP_USER_ID } =
   process.env;
 
 const Contact = () => {
+  const [validated, setValidated] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
   //Animate on scroll
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
 
   //Email JS
-  const form = useRef();
-  const [isLoading, setIsLoading] = useState(false);
+  const formRef = useRef();
 
-  const sendEmail = (e) => {
+  const handleSendEmail = (e) => {
     e.preventDefault();
     setIsLoading(true);
-    emailjs
-      .sendForm(
-        REACT_APP_SERVICE_ID,
-        REACT_APP_TEMPLATE_ID,
-        REACT_APP_USER_ID,
-        form.current
-      )
-      .then(
-        () => {
-          toast.success("Message sent successfully");
-          setIsLoading(false);
-        },
-        () => {
-          toast.error("Failed to send message, please try again");
-        }
-      );
+    setValidated(true);
+    if (formRef === null) {
+      return;
+    }
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+      setIsLoading(false);
+    } else {
+      emailjs
+        .sendForm(
+          REACT_APP_SERVICE_ID,
+          REACT_APP_TEMPLATE_ID,
+          formRef.current,
+          REACT_APP_USER_ID
+        )
+        .then(
+          () => {
+            toast.success("Message sent successfully");
+            setIsLoading(false);
+            setValidated(false);
+            e.target.reset();
+          },
+          () => {
+            toast.error("Failed to send message, please try again");
+            setIsLoading(false);
+          }
+        );
+    }
   };
 
   return (
@@ -46,60 +62,73 @@ const Contact = () => {
       <p className="contact_header_text text-center" data-aos="fade-up">
         GET IN TOUCH
       </p>
-      <Form
-        data-aos="fade-up"
-        ref={form}
-        className="contact_form text-dark"
-        onSubmit={sendEmail}
-      >
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter name..."
-            name="user_name"
-            required
-          />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email..."
-            name="user_email"
-            required
-          />
-        </Form.Group>
-        <Form.Label>Message</Form.Label>
-        <FloatingLabel
-          controlId="floatingTextarea"
-          label="Your message..."
-          className="mb-3"
+      <div className="form_wrapper">
+        <Form
+          noValidate
+          validated={validated}
+          data-aos="fade-up"
+          ref={formRef}
+          className="contact_form text-dark"
+          onSubmit={handleSendEmail}
         >
-          <Form.Control
-            as="textarea"
-            placeholder="Leave a comment here"
-            name="message"
-            required
-          />
-        </FloatingLabel>
-        <p className="feedback_text">I appreciate your feedback!</p>
-        <div className="send_btn_wrapper">
-          <button
-            value="Send"
-            type="submit"
-            className="send_btn"
-            disabled={isLoading}
-          >
-            {isLoading ? "Sending..." : "Submit"}
-            <RiSendPlane2Line
-              size={20}
-              style={{ margin: "3px", marginLeft: "5px" }}
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter name..."
+              name="user_name"
+              required
             />
-          </button>
-        </div>
-      </Form>
+            <Form.Control.Feedback type="invalid">
+              Please enter your name
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email..."
+              name="user_email"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter a valid email
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Label>Message</Form.Label>
+          <FloatingLabel
+            controlId="floatingTextarea"
+            label="Your message..."
+            className="mb-3"
+          >
+            <Form.Control
+              as="textarea"
+              placeholder="Leave a comment here"
+              name="message"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter a message
+            </Form.Control.Feedback>
+          </FloatingLabel>
+          <p className="feedback_text">I appreciate your feedback!</p>
+          <div className="send_btn_wrapper">
+            <button
+              value="Send"
+              type="submit"
+              className="send_btn"
+              disabled={isLoading}
+            >
+              {isLoading ? "Sending..." : "Submit"}
+              <RiSendPlane2Line
+                size={20}
+                style={{ margin: "3px", marginLeft: "5px" }}
+              />
+            </button>
+          </div>
+        </Form>
+      </div>
     </Container>
   );
 };
